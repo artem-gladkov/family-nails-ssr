52a8ca49adfe6bd19e8f875ce838b7efc2e876c7
Artem Gladkov <artem.gladkovw@gmail.com>
2022-10-07 17:18:55 +0300

    Fix seo bug property => name

--- shared/ui/PageLayout/PageLayout.tsx
+++ shared/ui/PageLayout/PageLayout.tsx
@@ -20,7 +20,7 @@ export const PageLayout: FC<PageLayoutProps> = ({pageTitle,description,  childre
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
 
         <title>{pageTitle}</title>
-        <meta property="description" content={description} />
+        <meta name="description" content={description} />
       </Head>
 
       <Header/>
